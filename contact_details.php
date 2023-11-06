c4846115131aff0079ff6a3120898b5d554a0633
Stnply2 <149630905+Stnply2@users.noreply.github.com>
2023-11-06 23:00:00 +0100

    Create contact_details.php

---
+++ contact_details.php
@@ -0,0 +1,271 @@
+<?php
+require_once('config.php');
+
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+
+if (!isset($_GET['id'])) {
+    die("Keine Kontakt-ID angegeben.");
+}
+
+$contactId = $_GET['id'];
+
+$stmt = $pdo->prepare("SELECT * FROM contacts WHERE id = ?");
+$stmt->execute([$contactId]);
+$contact = $stmt->fetch();
+
+if (!$contact) {
+    die("Kontakt nicht gefunden.");
+}
+
+$fullPhoneNumber = $contact['country_code'] . ' ' . $contact['area_code'] . ' ' . $contact['phone'];
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kontaktdetails</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <style>
+        /* Ihr CSS-Code hier */
+        body {
+            background-color: #f4f4f4;
+        }
+        .mdl-card {
+            border-radius: 15px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+        }
+        .contact-info {
+            font-size: 24px;
+            text-align: center;
+        }
+        .edit-icon {
+            color: #555;
+            position: absolute;
+            right: 15px;
+            top: 15px;
+        }
+        .delete-icon {
+            color: red;
+            position: absolute;
+            right: 50px;
+            top: 15px;
+            display: none; /* Versteckt den Mülleimer-Icon zu Beginn */
+        }
+        .mdl-button--blue {
+            color: blue;
+            border: 1px solid blue;
+            border-radius: 15px;
+            margin-top: 20px;
+            display: block;
+            width: 80%;
+            margin-left: auto;
+            margin-right: auto;
+        }
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+            border-radius: 50%;
+            margin-top: 20px;
+            margin-bottom: 10px;
+            width: 150px;
+            height: 150px;
+        }
+        p {
+            text-align: center;
+            font-size: 18px;
+        }
+        h2 {
+            text-align: center;
+            margin-bottom: 10px;
+        }
+        /* CSS für das Modal */
+        .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+        }
+        /* Modal Content (Image) */
+   .modal-content {
+    display: block;
+	 border-radius: 0; 
+    max-width: 700px; /* Maximale Breite des Bildes */
+    width: 25%; /* Setzen Sie die Breite des Bildes auf 50% für größere Bildschirme, es wird aber nicht breiter als 700px sein. */
+    margin: 15% auto; /* 15% von oben und zentriert */
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19); /* Optional: Schatten für das Bild */
+}
+
+    .modal {
+        /* andere Stile beibehalten */
+        display: flex; /* Verwendung von Flexbox für die Zentrierung */
+        align-items: center; /* vertikal zentrieren */
+        justify-content: center; /* horizontal zentrieren */
+    }
+        /* Add Animation - Zoom in the Modal */
+        .modal-content, #caption { 
+            animation-name: zoom;
+            animation-duration: 0.6s;
+        }
+        @keyframes zoom {
+            from {transform:scale(0)}
+            to {transform:scale(1)}
+        }
+        /* The Close Button */
+        .close {
+            position: absolute;
+            top: 15px;
+            right: 35px;
+            color: #f1f1f1;
+            font-size: 40px;
+            font-weight: bold;
+            transition: 0.3s;
+        }
+        .close:hover,
+        .close:focus {
+            color: #bbb;
+            text-decoration: none;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+<div class="mdl-layout mdl-js-layout">
+    <main class="mdl-layout__content">
+        <div class="mdl-card mdl-shadow--2dp" style="margin: 2em; padding: 2em;" id="details">
+            <!-- Image used to trigger the modal -->
+            <img id="myImg" src="<?php echo !empty($contact['image']) ? $contact['image'] : 'server/contacts.png'; ?>" alt="Kontaktbild">
+            <h2 class="contact-info">
+                <?php echo $contact['salutation'] . ' ' . $contact['first_name'] . ' ' . $contact['last_name']; ?>
+                <button onclick="toggleEdit()" class="mdl-button mdl-js-button mdl-button--icon">
+                    <i class="material-icons edit-icon">edit</i>
+            </button>
+            </h2>
+            <p>Firma: <?php echo $contact['company']; ?></p>
+            <p>Email: <?php echo $contact['email']; ?></p>
+            <p>Telefonnummer: <?php echo $fullPhoneNumber; ?></p>
+            <p>Adresse: <?php echo $contact['address']; ?></p>
+			<p><?php echo $contact['address2']; ?></p>
+            <a href="index.php" class="mdl-button mdl-js-button mdl-button--blue">Zurück zur Kontaktliste</a>
+        </div>
+
+        <div class="mdl-card mdl-shadow--2dp" style="margin: 2em; padding: 2em; display:none;" id="editForm">
+            <button onclick="confirmDelete()" class="mdl-button mdl-js-button mdl-button--icon">
+                <i class="material-icons delete-icon">delete</i>
+            </button>
+            <form action="update_contact.php" method="post" enctype="multipart/form-data">
+                <div class="mdl-textfield mdl-js-textfield">
+                    Anrede: <input class="mdl-textfield__input" type="text" name="salutation" value="<?php echo $contact['salutation']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Vorname: <input class="mdl-textfield__input" type="text" name="first_name" value="<?php echo $contact['first_name']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Nachname: <input class="mdl-textfield__input" type="text" name="last_name" value="<?php echo $contact['last_name']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Firma:
+                    <input class="mdl-textfield__input" type="text" name="company" value="<?php echo $contact['company']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Email: <input class="mdl-textfield__input" type="text" name="email" value="<?php echo $contact['email']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Telefonnummer: <input class="mdl-textfield__input" type="text" name="phone" value="<?php echo $contact['phone']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Landesvorwahl: <input class="mdl-textfield__input" type="text" name="country_code" value="<?php echo $contact['country_code']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Ortsvorwahl: <input class="mdl-textfield__input" type="text" name="area_code" value="<?php echo $contact['area_code']; ?>">
+                </div>
+				 <div class="mdl-textfield mdl-js-textfield">
+                    Straße: <input class="mdl-textfield__input" type="text" name="street" value="<?php echo $contact['street']; ?>">
+                </div>
+				 <div class="mdl-textfield mdl-js-textfield">
+                    Hausnummer: <input class="mdl-textfield__input" type="text" name="house_number" value="<?php echo $contact['house_number']; ?>">
+                </div>
+				 <div class="mdl-textfield mdl-js-textfield">
+                    Postleitzahl: <input class="mdl-textfield__input" type="text" name="postal_code" value="<?php echo $contact['postal_code']; ?>">
+                </div>
+				<div class="mdl-textfield mdl-js-textfield">
+                    Ort: <input class="mdl-textfield__input" type="text" name="city" value="<?php echo $contact['city']; ?>">
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    Bild:(Max. 5MB, JPEG, PNG, GIF, SVG) <input class="mdl-textfield__input" type="file" name="image">
+                    <img src="<?php echo !empty($contact['image']) ? $contact['image'] : 'server/contacts.png'; ?>" alt="Kontaktbild" style="width:100px;">
+                </div>
+                <input type="hidden" name="id" value="<?php echo $contactId; ?>">
+                <button type="submit" class="mdl-button mdl-js-button mdl-button--blue">Speichern</button>
+            </form>
+        </div>
+    </main>
+</div>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+    <span class="close">&times;</span>
+    <img class="modal-content" id="img01">
+    <div id="caption"></div>
+</div>
+
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    var img = document.getElementById("myImg");
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    var span = document.getElementsByClassName("close")[0];
+
+    // Nur wenn 'myImg' existiert, fügen Sie den Klick-Event-Listener hinzu.
+if (img) {
+    img.onclick = function() {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
+
+    if (span) {
+        // Wenn das (x) angeklickt wird, schließen Sie das Modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+    }
+});
+    function toggleEdit() {
+        var details = document.getElementById('details');
+        var editForm = document.getElementById('editForm');
+        var deleteIcon = document.querySelector('.delete-icon');
+
+        if (editForm.style.display === 'none') {
+            editForm.style.display = 'block';
+            details.style.display = 'none';
+            deleteIcon.style.display = 'block';
+        } else {
+            editForm.style.display = 'none';
+            details.style.display = 'block';
+            deleteIcon.style.display = 'none';
+        }
+    }
+
+    function confirmDelete() {
+        var confirmation = confirm("Sind Sie sicher, dass Sie diesen Kontakt löschen möchten?");
+        if (confirmation) {
+            window.location.href = 'delete_contact.php?id=<?php echo $contactId; ?>';
+        }
+    }
+</script>
+</body>
+</html>
